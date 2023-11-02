176334463da492ce37b978b8e4d4898b7ed3d379
CodeSandbox <support@codesandbox.io>
2023-11-02 10:49:50 +0000

    Initial commit

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const { Pool } = require("pg");
+const config = require("./config.js");
+
+const pool = new Pool(config.db);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -17,6 +21,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-var listener = app.listen(8080, function() {
+//TODO: how to handle single updates (e.g. activate device)?
+//TODO: how to update priority (whole list needs to be updated)
+app.route("/api/devices/").get();
+app.route("api/device/:id").get().put();
+app.route("api/data").get();
+
+var listener = app.listen(3000, function () {
   console.log("Listening on port " + listener.address().port);
 });
