b0589924d3d1505b54b4c66c4ed66baa068b12b9
Lucas Graf <graflucas97@gmail.com>
2023-11-04 12:31:53 +0200

    added config file with quest-db configuration && added basic api endpoints

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const { Pool } = require("pg");
 const config = require("./config.js");
+const { Client } = require("pg")
+
 
 const pool = new Pool(config.db);
 
@@ -23,13 +25,23 @@ app.use("/users", usersRouter);
 
 //TODO: how to handle single updates (e.g. activate device)?
 //TODO: how to update priority (whole list needs to be updated)
-app.route("/api/devices/").get((req, res) => {
-  pool.query("SELECT * from devices").then((devices) => {
-    res.json(devices);
-  });
+app.route("/api/devices/").get(async (req, res) => {
+  var devices = await pool.query('SELECT * from devices;');
+  res.json({devices:devices.rows})
 });
-app.route("api/device/:id").get().put();
-app.route("api/data").get();
+
+app.route("/api/device/:id").get(async (req, res) => {
+  var device = await pool.query('SELECT * from devices where id= $1', [req.params.id]);
+  res.json({device: device.rows[0]});
+}).put(async (req, res) => {
+  var {device_name, priority, consumption, active_management, active, flexible_consumption} = req.body;
+  var updatedDevice = await pool.query("UPDATE devices SET device_name=$1, priority=$2, consumption=$3, active_management=$4, active=$5, flexible_consumption=$6  where id=$7",
+   [device_name, priority, consumption, active_management, active, flexible_consumption, req.params.id]);
+   res.json({device: updatedDevice});
+   //TODO: update response
+
+});
+app.route("/api/data").get().post();
 
 var listener = app.listen(3000, function () {
   console.log("Listening on port " + listener.address().port);
--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const config = {
   db: {
     /* do not put password or any sensitive info here, done only for demo */
     host: env.DB_HOST || "localhost",
-    port: env.DB_PORT || "9000",
-    user: env.DB_USER || "",
-    password: env.DB_PASSWORD || "",
-    database: env.DB_NAME || "",
+    port: env.DB_PORT || "8812",
+    user: env.DB_USER || "admin",
+    password: env.DB_PASSWORD || "quest",
+    database: env.DB_NAME || "qdp",
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
